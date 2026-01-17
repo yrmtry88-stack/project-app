b5640eee8d6101bf3746295334da43c536f3445f
yr817 <yr817@hivelab.co.kr>
2026-01-17 23:28:45 +0900

    [260117] first update

---
+++ myMatzipApp/.eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  root: true,
+  extends: '@react-native',
+  "@typescript-eslint/no-unused-vars": "off"
+};
---
+++ myMatzipApp/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+function App(){
+  return (
+    <View></View>
+  );
+}
+const styles = StyleSheet.create({});
+
+export default App;
